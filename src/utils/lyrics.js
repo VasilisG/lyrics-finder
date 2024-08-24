867d377a2c24f746f4debe6405c5bacee48c9861
Vasilis <goutz@softweb.gr>
2024-08-24 15:34:32 +0300

    Changes in url character escaping.

--- src/utils/lyrics.js
+++ src/utils/lyrics.js
@@ -1,5 +1,10 @@
 import * as DOMPurify from 'dompurify';
 
 export const formatLyrics = (lyrics) => {
-  return DOMPurify.sanitize(lyrics.replace(/(?:\r\n|\r|\n)/g, '<br>'));
+  return DOMPurify.sanitize(
+    encodeURIComponent(lyrics)
+    .replace('!', 'i')
+    .replace(/\*/g, "%2A")
+    .replace(/(?:\r\n|\r|\n)/g, '<br>')
+  );
 }
